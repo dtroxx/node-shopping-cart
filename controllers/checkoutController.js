ee6cb231e442d21e99a6aabb5a334b900802f998
dtroxx <dtroxx@gmail.com>
2017-07-18 19:27:56 -0700

    updated empty cart flash message, fixed eslint errors

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -4,8 +4,8 @@ const Order = require('../models/Order');
 
 exports.getCheckout = (req, res) => {
   if (!req.session.cart || req.session.cart.totalQty === 0) {
-    req.flash('warning', 'Your cart is empty.');
-    return res.redirect('/shopping-cart');
+    req.flash('warning', 'Your cart is empty. Please add products to cart before checking out.');
+    res.redirect('/shopping-cart');
   }
   const cart = new Cart(req.session.cart);
   const errMsg = req.flash('error')[0]; // stores any errors from stripe creating charge
@@ -13,11 +13,11 @@ exports.getCheckout = (req, res) => {
 };
 
 exports.postCheckout = (req, res) => {
-  if (!req.session.cart || req.session.cart.totalQty === 0) {    
-    return res.redirect('/shopping-cart');
+  if (!req.session.cart || req.session.cart.totalQty === 0) {
+    res.redirect('/shopping-cart');
   }
   const cart = new Cart(req.session.cart);
-  
+
   const stripe = require("stripe")(
   "sk_test_RDtaLjxLJ8AV9ULqeH9gghtA"
 );
@@ -27,10 +27,10 @@ exports.postCheckout = (req, res) => {
     currency: 'usd',
     source: req.body.stripeToken, // obtained with Stripe.js passed via form in hidden input
     description: 'Test Charge'
-}, function(err, charge) {
+  }, (err, charge) => {
     if (err) {
-        req.flash('error', err.message);
-        return res.redirect('/checkout');
+      req.flash('error', err.message);
+      return res.redirect('/checkout');
     }
     const order = new Order({
       user: req.user,
@@ -39,9 +39,9 @@ exports.postCheckout = (req, res) => {
       name: req.body.name,
       paymentId: charge.id  // passed to callback of stripe.charges.create
     });
-    order.save(function(err, result){ // save order to database
-      if (err) {
-        console.log(err);
+    order.save((error, result) => { // save order to database
+      if (error) {
+        console.log(error);
       }
       req.flash('success', 'Purchase Successful!');
       req.session.cart = null; // clear the cart
